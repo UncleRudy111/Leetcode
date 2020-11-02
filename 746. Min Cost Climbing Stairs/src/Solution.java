3db7bc6a976119645a98b5fa9ee1c5846dbcfac5
cherry-rabbit <70192755+cherry-rabbit@users.noreply.github.com>
2020-11-02 10:48:20 +0800

    746. Min Cost Climbing Stairs

--- 746. Min Cost Climbing Stairs/src/Solution.java
+++ 746. Min Cost Climbing Stairs/src/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
 
         int[] dp = new int[cost.length];
         dp[0] = cost[0];
-        dp[1] = Math.min(cost[1],cost[0]);
+        dp[1] = cost[1];
         for(int i=2;i<cost.length;i++){
             dp[i] = Math.min(dp[i-1],dp[i-2]) + cost[i];
         }
